ee5a68a6056e2e003f4bb51869819ecf6cb4c52a
Will Fairclough <wfairclough@gmail.com>
2023-04-02 15:10:49 -0400

    Done 'options'

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,8 +1,7 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
@@ -13,7 +12,9 @@ fn main() {
 
     match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => println!("no match"),
+        // or if you don't want to implement Copy
+        // Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        _ => panic!("no match"),
     }
     y; // Fix without deleting this line.
 }
