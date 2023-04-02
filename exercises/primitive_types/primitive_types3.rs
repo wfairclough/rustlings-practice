e64cfc37747bd1d9c55d77ac615a31d3ca2589e2
Will Fairclough <wfairclough@gmail.com>
2023-04-02 12:41:55 -0400

    Exercies completed: primitive_types, vecs, structs, if

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,13 +2,14 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let range = 0..101;
+    // check the type of range and print it
+    println!("range is of type {:?}", range);
+    let a: String = range.map(|x| x.to_string()).collect::<Vec<String>>().join(",");
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array!: {}", a);
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
     }
